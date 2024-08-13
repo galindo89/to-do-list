95382f92732ee89910847e36a22ec43e6c9033ed
Pablo <pablo@livedooh.com>
2024-08-13 16:11:19 +0200

    adding basic css to the modal container

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,22 @@ console.log('Hello, world!');
 
 document.addEventListener('DOMContentLoaded', function() {
     console.log('The page is fully loaded');
+
+    const createTaskBtn = document.getElementById('createTaskBtn');
+    const modal = document.getElementById('modal');
+    const closeModal = document.getElementById('closeModal');
+    const addTaskBtn = document.getElementById('addTaskBtn');
+    const modalTaskInput = document.getElementById('modalTaskInput');
+    const mondalTaskDate = document.getElementById('modalTaskDate');
+    const modalTaskStatus = document.getElementById('modalTaskStatus');
+
+
+//Creating an event listener that listens for a click on the create task button. This will show the modal
+
+createTaskBtn.addEventListener('click', function() {
+    modal.style.display = 'block';
+}
+
+);
+
 });
