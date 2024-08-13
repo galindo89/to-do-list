b4c12d4ccbf3c03c4f7472c00ffd12251ac617b8
Pablo <pablo@livedooh.com>
2024-08-13 15:49:15 +0200

    add event listener that gets fired when the whole HTML loads

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
 console.log('Hello, world!');
+
+//Creating an event listener that loads when the whole page is loaded
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('The page is fully loaded');
+});
