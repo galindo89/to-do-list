30302b53e652cc4a5bd79db355605e9f738dc9ce
Pablo <pablo@livedooh.com>
2024-08-15 22:47:01 +0200

    adding dynamically input elements to allow the update of task, status and due date. Update query selectors for save task function. Adding event listeners to allow the update of tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -80,12 +80,44 @@ document.addEventListener('DOMContentLoaded', function () {
     //Creating a function that will add a task to the task list
 
     function createTaskItem(taskText, dueDate, status) {
+
         const taskItem = document.createElement('li');
         const taskContent = document.createElement('div');
         taskContent.classList.add('task-content');
+
+       //text input for the task
         const taskTextSpan = document.createElement('span');
+        const taskTextInput = document.createElement('input');
+        taskTextSpan.textContent = taskText;
+        taskTextInput.type = 'text';
+        taskTextInput.value = taskText;
+        taskTextInput.style.display = 'none';
+        //due date for the task
         const taskDueDateSpan = document.createElement('span');
+        const taskDueDateInput = document.createElement('input');
+        taskDueDateSpan.textContent = dueDate;
+        taskDueDateInput.type = 'date';
+        taskDueDateInput.value = dueDate;
+        taskDueDateInput.style.display = 'none';
+        //status of the task
         const statusSpan = document.createElement('span');
+        const statusSelect = document.createElement('select');
+        statusSpan.textContent = status;
+        const options =    
+        [{"option":"Backlog","value":'backlog'}, 
+         {"option":"In Progress","value":'in-progress'},
+         {"option":"Done","value":'done'}];
+
+         options.forEach(option => {
+            const statusOption = document.createElement('option');
+            statusOption.textContent = option.option;
+            statusOption.value = option.value;
+            statusSelect.appendChild(statusOption);
+        });
+        statusSelect.value = status;
+        statusSelect.style.display = 'none';
+             
+       //delete task button 
         const deleteTask = document.createElement('div');
         deleteTask.classList.add('delete-task');
         const deleteTaskBtn = document.createElement('i');
@@ -97,18 +129,74 @@ document.addEventListener('DOMContentLoaded', function () {
 
         changeTaskColor(taskItem, status, daysLeft);
 
-        //Changing the background color of the task based on the number of days left to complete the task
-
-      
-        taskTextSpan.textContent = taskText;
-        taskDueDateSpan.textContent = dueDate;
-        statusSpan.textContent = status;
+        //Appending the elements to their corresponding parent elements     
+       
         taskContent.appendChild(taskTextSpan);
+        taskContent.appendChild(taskTextInput);
         taskContent.appendChild(taskDueDateSpan);
+        taskContent.appendChild(taskDueDateInput);
         taskContent.appendChild(statusSpan);
+        taskContent.appendChild(statusSelect);
         taskItem.appendChild(taskContent);
         deleteTask.appendChild(deleteTaskBtn);
         taskItem.appendChild(deleteTask);
+
+       
+        //Event listener to edit the task text
+
+        taskTextSpan.addEventListener('click', function () {
+
+            taskTextSpan.style.display = 'none';
+            taskTextInput.style.display = 'inline-block';
+            taskTextInput.focus();
+           
+        })
+
+        taskTextInput.addEventListener('blur', function () {
+            taskTextSpan.textContent = taskTextInput.value;
+            taskTextSpan.style.display = 'inline-block';
+            taskTextInput.style.display = 'none';
+            saveTasksToLocalStorage();
+        });
+
+        //Event listener to edit the due date
+
+        taskDueDateSpan.addEventListener('click', function () {
+
+            taskDueDateSpan.style.display = 'none';
+            taskDueDateInput.style.display = 'inline-block';
+            taskDueDateInput.focus();
+        })
+
+        taskDueDateInput.addEventListener('blur', function () {
+            taskDueDateSpan.textContent = taskDueDateInput.value;
+            taskDueDateSpan.style.display = 'inline-block';
+            taskDueDateInput.style.display = 'none';
+            changeTaskColor(taskItem, status, calculateDaysLeft(taskDueDateInput.value));
+            saveTasksToLocalStorage();
+        });
+
+        //Event listener to edit the status
+
+        statusSpan.addEventListener('click', function () {
+
+            statusSpan.style.display = 'none';
+            statusSelect.style.display = 'inline-block';
+            statusSelect.focus();
+        });
+
+        statusSelect.addEventListener('change', function () {
+            statusSpan.textContent = statusSelect.value;
+            statusSpan.style.display = 'inline-block';
+            addTaskToColumn(taskItem, statusSelect.value);
+            statusSelect.style.display = 'none';
+            saveTasksToLocalStorage();
+           
+         
+           
+        });
+    
+        //Event listener to delete the task
 
         deleteTaskBtn.addEventListener('click', function () {
 
@@ -138,14 +226,15 @@ document.addEventListener('DOMContentLoaded', function () {
         const tasks = [];
         document.querySelectorAll('.column ul li').forEach(taskItem => {
             const taskContent = taskItem.querySelector('.task-content');
-            const text = taskContent.querySelector('span:nth-child(1)').textContent.trim();
-            const dueDate = taskContent.querySelector('span:nth-child(2)').textContent;
-            const status = taskContent.querySelector('span:nth-child(3)').textContent;
+            
+            const text = taskContent.querySelector('input[type="text"]').value.trim();
+            const dueDate = taskContent.querySelector('input[type="date"]').value;
+            const status = taskContent.querySelector('select').value;
+    
             tasks.push({ text, dueDate, status });
         });
         localStorage.setItem('tasks', JSON.stringify(tasks));
     }
-
 
    
 
